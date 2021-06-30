3a639e6b4034d35214495ba64a386ce5e3f535ef
liwuxin <1184144908@qq.com>
2021-06-30 11:04:55 +0800

    网上交易系统项目初搭建

---
+++ src/main/java/com/cdtu/ots/OtsApplication.java
@@ -0,0 +1,13 @@
+package com.cdtu.ots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cdtu/ots/OtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdtu.ots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
