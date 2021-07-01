14f856352b192c84bf650a27f8369fd999eed6fe
Mars-Sea <1195751600@qq.com>
2021-07-01 16:17:28 +0800

    update application info

--- src/main/java/com/cdtu/ots/controller/HomeController.java
+++ src/main/java/com/cdtu/ots/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.cdtu.ots.Controller;
+package com.cdtu.ots.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
