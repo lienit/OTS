75a8ef488e956a2ba35d18560e2352de61eee264
Mars-Sea <1195751600@qq.com>
2021-07-01 16:58:37 +0800

    test filter

--- src/main/java/com/cdtu/ots/filter/HomeFilter.java
+++ src/main/java/com/cdtu/ots/filter/HomeFilter.java
@@ -1,10 +1,35 @@
 package com.cdtu.ots.filter;
 
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
 /**
  * @ClassName HomeFilter
  * @Description TODO
  * @Author mars_sea
  * @Date 2021/7/1
  **/
-public class HomeFilter {
+@Component
+@WebFilter(urlPatterns = "/*", filterName = "homeFilter")
+public class HomeFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("嘿嘿!");
+        // 放行
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
 }
