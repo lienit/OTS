d16f6383a3bf4a07f9c6334d1fb8ff238cd9eaa7
Mars-Sea <1195751600@qq.com>
2021-07-07 11:52:22 +0800

    添加需改信息，新增信息

--- src/main/java/com/cdtu/ots/controller/BusinessManController.java
+++ src/main/java/com/cdtu/ots/controller/BusinessManController.java
@@ -1,19 +1,15 @@
 package com.cdtu.ots.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.cdtu.ots.entity.Goods;
 import com.cdtu.ots.mapper.GoodsMapper;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
-import java.util.Map;
 
 /**
  * @ClassName BusinessManController
@@ -27,31 +23,71 @@ public class BusinessManController {
     @Autowired
     GoodsMapper goodsMapper;
 
+    /**
+     * 打开商家管理页面
+     *
+     * @return 返回商家管理页面
+     */
     @RequestMapping("/businessCenter")
-    public String businessCenter(){
+    public String businessCenter() {
         return "/businessman/businessCenter";
     }
 
+    /**
+     * 获取商家商品信息
+     *
+     * @param dataPage 当前页数
+     * @param pageSize 每页显示多少条数据
+     * @return 返回商品信息JSON格式
+     */
     @PostMapping("/getGoodsData")
     @ResponseBody
-    public String getGoodsData(int  dataPage,int pageSize){
+    public String getGoodsData(int dataPage, int pageSize) {
         int page = (dataPage - 1) * pageSize;
         ArrayList<Goods> goodsArrayList;
-        goodsArrayList = goodsMapper.findAllByStoreIdAndSize("123",page,pageSize);
+        goodsArrayList = goodsMapper.findAllByStoreIdAndSize("123", page, pageSize);
 
         return JSON.toJSONString(goodsArrayList);
     }
 
+    /**
+     * 获取当前商家商品总个数
+     *
+     * @return 商品个数
+     */
     @PostMapping("/getGoodsDataSize")
     @ResponseBody
-    public int getGoodsDataSize(){
+    public int getGoodsDataSize() {
         return goodsMapper.findSizeByStoreId("123");
     }
 
+    /**
+     * 删除商品信息
+     *
+     * @param gId 商品id
+     * @return 返回影响条目数
+     */
     @PostMapping("/deleteGoodsData")
     @ResponseBody
-    public int deleteGoodByGid(String gId){
+    public int deleteGoodByGid(String gId) {
         int err = goodsMapper.deleteAllBygID(gId);
         return err;
+    }
+
+    /**
+     * 修改商品信息
+     * @param goods 获取修改后商品信息
+     * @return 修改信息
+     */
+    @PostMapping("/updateGoodsData")
+    @ResponseBody
+    public int updateGoodsData(Goods goods){
+        return goodsMapper.updateGoodsDataByGid(goods);
+    }
+
+    @PostMapping("/addGoodsData")
+    @ResponseBody
+    public int addGoodsData(Goods goods){
+        return goodsMapper.addGoodData(goods);
     }
 }
--- src/main/java/com/cdtu/ots/mapper/GoodsMapper.java
+++ src/main/java/com/cdtu/ots/mapper/GoodsMapper.java
@@ -1,10 +1,7 @@
 package com.cdtu.ots.mapper;
 
 import com.cdtu.ots.entity.Goods;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.ArrayList;
 
@@ -17,13 +14,45 @@ import java.util.ArrayList;
 @Mapper
 public interface GoodsMapper {
 
+    /**
+     * 更具商家当前页面查询商品信息
+     * @param gStoreId 商家id
+     * @param page  当前页数
+     * @param size  每页显示条目数
+     * @return 物品list
+     */
     @Select("select * from Goods where gStoreId = #{gStoreId} LIMIT #{page},#{size}")
     public ArrayList<Goods> findAllByStoreIdAndSize(@Param("gStoreId") String gStoreId, @Param("page") int page,
                                              @Param("size") int size);
 
+    /**
+     * 查询商家共多少件商品
+     * @param gStoreId 商家id
+     * @return int 总数
+     */
     @Select("select count(gId) from Goods where gStoreId = #{gStoreId}")
     public int findSizeByStoreId(@Param("gStoreId") String gStoreId);
 
+    /**
+     * 删除商品信息
+     * @param gId 商品id
+     * @return 成功影响条目数
+     */
     @Delete("delete from Goods where gId = #{gId}")
     public int deleteAllBygID(@Param("gId") String gId);
+
+    /**
+     * 修改商品信息通过商品id
+     * @param goods 商品实体类信息
+     * @return 成功影响条目数
+     */
+    @Update("UPDATE Goods SET gCatId = #{goods.gCatId}, gName = #{goods.gName}, gImage = #{goods.gImage}, " +
+            "gPrice = #{goods.gPrice}, gParameter = #{goods.gParameter}, gCategory = #{goods.gCategory}" +
+            "where gId = #{goods.gId}")
+    public int updateGoodsDataByGid(@Param("goods") Goods goods);
+
+    @Insert("INSERT INTO Goods (gCatId, gStoreId, gName, gPrice, gImage, gParameter) " +
+            "VALUES(#{goods.gCatId}), #{goods.gStoreId}), #{goods.gName}),#{goods.gPrice})," +
+            "#{goods.gImage}), #{goods.gParameter})")
+    public int addGoodData(@Param("goods") Goods goodData);
 }
