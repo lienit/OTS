e8448f5fd970f6a5605bfd0964a3d5875f44258e
Mars-Sea <1195751600@qq.com>
2021-07-08 16:48:55 +0800

    添加统计图表，订单管理

--- src/main/java/com/cdtu/ots/filter/HomeFilter.java
+++ src/main/java/com/cdtu/ots/filter/HomeFilter.java
@@ -1,9 +1,15 @@
 package com.cdtu.ots.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.cdtu.ots.entity.User;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -13,7 +19,8 @@ import java.io.IOException;
  * @Date 2021/7/1
  **/
 @Component
-@WebFilter(urlPatterns = "/*", filterName = "homeFilter")
+@ServletComponentScan
+@WebFilter(urlPatterns = "/businessman/*", filterName = "businessmanFilter")
 public class HomeFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -23,9 +30,19 @@ public class HomeFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
                          FilterChain filterChain) throws IOException, ServletException {
-//        System.out.println("嘿嘿!");
-        // 放行
-        filterChain.doFilter(servletRequest,servletResponse);
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+        request.getSession(false);
+        User user = (User) request.getSession().getAttribute("user");
+        if (user != null && user.getuLevel() == "3"){
+            // 放行
+            filterChain.doFilter(servletRequest,servletResponse);
+        }else {
+            System.out.println("嘿嘿");
+            filterChain.doFilter(servletRequest,servletResponse);
+        }
+
     }
 
     @Override
---
+++ src/main/java/com/cdtu/ots/mapper/IndentMapper.java
@@ -0,0 +1,41 @@
+package com.cdtu.ots.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * @ClassName IndentMapper
+ * @Description TODO
+ * @Author mars_sea
+ * @Date 2021/7/8
+ **/
+@Mapper
+public interface IndentMapper {
+
+    @Select("SELECT  COUNT(i.iId) number, sum(i.iPrice) price, i.iDate time\n" +
+            "from indent i, `user` u, category c, goods g, store s \n" +
+            "WHERE i.iUserId = u.uId AND i.iCatId = c.cId AND i.iGoodsId = g.gId AND s.sId = #{sId} GROUP BY i.iDate")
+    public ArrayList<Map<String, Object>> getEventDayIndent(@Param("sId") String sId);
+
+    @Select("SELECT  COUNT(i.iId) number, sum(i.iPrice) price , c.cName \n" +
+            "from indent i, `user` u, category c, goods g, store s \n" +
+            "WHERE i.iUserId = u.uId AND i.iCatId = c.cId AND i.iGoodsId = g.gId AND s.sId = #{sId} GROUP BY c.cId")
+    public ArrayList<Map<String, Object>> getCategoryIndent(@Param("sId") String sId);
+
+    @Select("SELECT i.iId, i.iAddress, i.iMessage, i.iPrice, i.iDate, u.userName, c.cName, g.gName, s.sId ,i.iState\n" +
+            "from indent i, `user` u, category c, goods g, store s \n" +
+            "WHERE i.iUserId = u.uId AND i.iCatId = c.cId AND i.iGoodsId = g.gId AND s.sId = #{sId} LIMIT #{page},#{size}")
+    public ArrayList<Map<String, Object>> getIndentInfo(@Param("sId") String sId, @Param("page") int page,@Param("size") int size);
+
+    /**
+     * 查询商家共多少订单
+     * @param gStoreId 商家id
+     * @return int 总数
+     */
+    @Select("SELECT COUNT(iId) FROM indent WHERE iSotreId = #{gStoreId};")
+    public int findSizeByStoreId(@Param("gStoreId") String gStoreId);
+}
